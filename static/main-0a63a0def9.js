40dc5db1870a9508cd4c9e8798ead19913f35566
lanser.sia <lanser.sia@gmail.com>
2017-05-27 18:17:56 -0700

    removed alert

--- static/main-0a63a0def9.js
+++
@@ -1,4 +0,0 @@
-if (document.cookie.replace(/(?:(?:^|.*;\s*)underConstruction\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-    alert("This site is under construction.");
-    document.cookie = "underConstruction=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-}
