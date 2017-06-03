5510a36098a3932663512d99b4c725f2d153ca88
lanser-sia <lanser.sia@gmail.com>
2017-06-02 17:08:31 -0700

    try to load just font added type and rel to font css

--- _harp/gf.js
+++ _harp/gf.js
@@ -1,1 +1,1 @@
-(function(e){var o=e.createElement("link"),t=e.scripts[0];o.href="https://fonts.googleapis.com/css?family=PT+Sans",t.parentNode.insertBefore(o,t)})(document)
+(function(e){var o=e.createElement("link"),t=e.scripts[0];o.href="https://fonts.googleapis.com/css?family=PT+Sans";o.type="text/css";o.rel="stylesheet";t.parentNode.insertBefore(o,t)})(document)
